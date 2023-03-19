22f258037437a4255d8b83a5923fc0bfed77dfa2
Eliezer Barbosa <eliezerbs9@github.com>
2023-03-18 22:19:15 -0400

    fixed navbar

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,115 +1,111 @@
-import React, { useState, useEffect } from "react";
-import styled from "styled-components";
-import { AnimatePresence, motion } from "framer-motion";
-import SidebarLinks from "../SidebarLinks";
-import ArrowDown from "../ArrowDown";
-import ArrowSide from "../ArrowSide";
+import React, {useState, useEffect} from 'react';
+import styled from 'styled-components';
+import { AnimatePresence, motion } from 'framer-motion';
+import SidebarLinks from '../SidebarLinks';
+import ArrowDown from '../ArrowDown';
+import ArrowSide from '../ArrowSide';
 
 const StyledSidebar = styled(motion.aside)`
-  position: fixed;
-  top: 0;
-  left: 0;
-  bottom: 0;
-  background-color: rgba(24, 24, 25, 0.95);
-  height: 100vh;
-  overflow: hidden;
-  display: flex;
-  flex-direction: column;
-  z-index: 999;
-  padding: 20px;
-  gap: 50px;
-  justify-content: space-between;
-
-  .logo {
+    position: fixed;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    background-color: rgba(24,24,25,0.95);
+    height: 100vh;
+    overflow: hidden;
     display: flex;
-    justify-content: center;
-    align-items: center;
-    width: 100%;
-    img {
-      transition: ease-in 0.25s;
-      width: 100px;
-    }
-    padding-bottom: 20px;
-    border-bottom: 1px solid #e1e1e1;
-  }
-
-  .next {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-  }
-
-  @media screen and (max-width: 1024px) {
-    bottom: unset;
-    right: 0;
-    height: fit-content;
-    flex-direction: row;
-    align-items: center;
+    flex-direction: column;
+    z-index: 999;
     padding: 20px;
+    gap: 50px;
+    justify-content: space-between;
 
-    .logo {
-      height: fit-content;
-      border-bottom: none;
-      border-right: 1px solid #e1e1e1;
-      padding: 0;
-      width: 100%;
+
+    .logo{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 100%;
+        img{ 
+            transition: ease-in 0.25s;
+            width: 100px; 
+        }
+        padding-bottom: 20px;
+        border-bottom: 1px solid #e1e1e1;
     }
 
-    .next {
-      width: 100%;
+    .next{
+        display: flex;
+        align-items: center;
+        justify-content: center;
     }
-  }
 
-  @media screen and (max-width: 480px) {
-    gap: 5px;
-    padding: 5px;
-    .logo {
-      width: fit-content;
-      padding: 0 10px;
-      img {
-        width: 70px;
-      }
+    @media screen and (max-width: 1024px){
+        bottom: unset;
+        right: 0;
+        height: fit-content;
+        flex-direction: row;
+        align-items: center;
+        padding: 20px;
+
+        .logo{ 
+            height: fit-content;
+            border-bottom: none; 
+            border-right: 1px solid #e1e1e1;
+            padding: 0; 
+            width: 100%;
+        }
+
+        .next{
+            width: 100%;
+        }
     }
-    .next {
-      width: fit-content;
-      padding: 0 10px;
+
+    @media screen and (max-width: 480px){
+        gap: 5px; 
+        padding: 5px;
+        .logo {
+            width: fit-content;
+            padding: 0 10px;
+            img{
+                width: 70px;
+            }
+        }
+        .next{
+            width: fit-content;
+            padding: 0 10px;
+        }
     }
-  }
 `;
 
-const Sidebar = ({ isMobile }) => {
-  const [expanded, setExpanded] = useState(false);
+const Sidebar = ({isMobile}) => {
+    const [expanded, setExpanded] = useState(false);
+    
 
-  return (
-    <StyledSidebar
-      onHoverStart={() => !isMobile && setExpanded(true)}
-      onHoverEnd={() => !isMobile && setExpanded(false)}
-      initial={{
-        width: expanded ? "100%" : "auto",
-      }}
-      animate={{
-        width: expanded ? "100%" : "auto",
-      }}
-      transition={{
-        duration: 0.5,
-      }}
-    >
-      <div className="logo">
-        <motion.img
-          transition={{ duration: 0.2, ease: "ease-in" }}
-          src={require("../../assets/logo.png").default}
-          alt="logo"
-        />
-      </div>
+    return (
+        <StyledSidebar onHoverStart={() => !isMobile && setExpanded(true)} onHoverEnd={() => !isMobile && setExpanded(false)}
+            initial={{
+                width: expanded ? '300px' : 'auto'
+            }}
+            animate={{
+                width: expanded ? '300px' : 'auto'
+            }}
+            transition={{
+                duration: 0.5
+            }}
+        >
+            <div className="logo">
+                <motion.img transition={{duration: 0.2, ease: 'ease-in'}} src={require('../../assets/logo.png').default} alt='logo'/>
+            </div>
+            
+            <SidebarLinks expanded={expanded} isMobile={isMobile} />
 
-      <SidebarLinks expanded={expanded} isMobile={isMobile} />
+            <div className="next">
+                <ArrowDown />
+                <ArrowSide />
+            </div>
+        </StyledSidebar>
+    )
+}
 
-      <div className="next">
-        <ArrowDown />
-        <ArrowSide />
-      </div>
-    </StyledSidebar>
-  );
-};
-
-export default Sidebar;
+export default Sidebar
